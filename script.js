9ddf2ef6e445577b3ef2fff7279b53f5f0d45b54
marksgilligan <59980448+marksgilligan@users.noreply.github.com>
2020-01-31 15:09:53 +0000

    first JS version fin

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-// category: "Animals"
-// type: "boolean"
-// difficulty: "easy"
-// question: "The Axolotl is an amphibian that can spend its whole life in a larval state."
-// correct_answer: "True"
-// incorrect_answers: ["False"]
+function newQuestion() {
+  fetch("https://opentdb.com/api.php?amount=10&category=27&type=boolean")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      document.querySelector("#question").innerText = data.results[0].question;
+      const incorrect = (document.querySelector("#incorrect").innerText =
+        data.results[0].incorrect_answers);
+      const correct = (document.querySelector("#correct").innerText =
+        data.results[0].correct_answer);
+      return correct, incorrect;
+    });
+}
 
-fetch("https://opentdb.com/api.php?amount=10&category=27&type=boolean")
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    console.log(data);
-    document.querySelector("#question").innerText = data.results[0].question;
-    document.querySelector("#incorrect").innerText =
-      data.results[0].incorrect_answers;
-    document.querySelector("#correct").innerText =
-      data.results[0].correct_answer;
-  });
+function answer(correct) {
+  document.querySelector("#answer").innerText = correct;
 
-function answer(ans) {
-  if (ans == "True") {
+  if (correct == "True") {
     document.querySelector("#answer").innerText = "TRUE!";
+    dogbark.Play();
   }
+
   document.querySelector("#answer").innerText = "FALSE!";
+  toy.Play();
 }
